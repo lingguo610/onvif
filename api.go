df5b55adf12c6259abbccca4189d871306967223
ling.guo 郭玲 <160718989@Hytera.com>
2023-05-08 18:03:17 +0800

    update

--- api.go
+++ api.go
@@ -1,19 +1,4 @@
-/*
- * @Author: error: git config user.name && git config user.email & please set dead value or install git
- * @Date: 2022-07-25 13:49:49
- * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-07-26 09:43:16
- * @FilePath: \onvif\api.go
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-/*
- * @Author: error: git config user.name && git config user.email & please set dead value or install git
- * @Date: 2022-07-25 13:31:31
- * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-07-25 16:21:18
- * @FilePath: \go-onvif\api.go
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+
 package onvif
 
 import "github.com/lingguo610/onvif/device"
